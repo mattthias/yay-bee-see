282a20401e981555bd7d062abb015acc14363486
Wade Brainerd <wadetb@gmail.com>
2008-12-02 15:41:39 +0000

    Tweaks.

--- yaybeesee.py
+++ yaybeesee.py
@@ -17,7 +17,7 @@
 """YayBeeSee - Simple letter game."""
 
 # Import standard Python modules.
-import logging, os, math, time, copy, json
+import logging, os, json, locale, random
 from gettext import gettext as _
 
 # Import PyGTK.
@@ -51,9 +51,10 @@ class YayBeeSee(sugar.activity.activity.Activity):
         tbox = sugar.activity.activity.ActivityToolbox(self)
         self.set_toolbox(tbox)
         
-        # Load the image index.
+        # Load the image index for the current locale.
         bundle = sugar.activity.activity.get_bundle_path()
-        fd = open(bundle + '/images/INDEX', 'r')
+        code = locale.getlocale(locale.LC_ALL)[0]
+        fd = open(bundle + '/images/INDEX.'+code, 'r')
         self.index = json.read(fd.read())
 
         # This has to happen last, because it calls the read_file method when restoring from the Journal.
@@ -68,6 +69,10 @@ class YayBeeSee(sugar.activity.activity.Activity):
         self.add_events(gtk.gdk.KEY_PRESS_MASK)
         self.connect('key-press-event', self.key_press_cb)
 
+        # Set up the 'Ken Burns' timer.
+        self.reset_zoom()
+        #gobject.idle_add(self.idle_cb)
+
     def key_press_cb(self, widget, event):
         # Get the letter corresponding to the keypress.
         key = event.string.lower()
@@ -80,10 +85,28 @@ class YayBeeSee(sugar.activity.activity.Activity):
             bundle = sugar.activity.activity.get_bundle_path()
             filename = os.path.join(bundle, self.key_info['file'])
             self.pixbuf = gtk.gdk.pixbuf_new_from_file(filename)
-        
+
+        self.reset_zoom()
+
         self.area.queue_draw()
         
-        return False
+        return True
+
+    def reset_zoom(self):
+        self.zoom = 1.0
+        self.px = 0.0
+        self.py = 0.0
+        self.vx = random.random() * 0.02 - 0.01
+        self.vy = random.random() * 0.02 - 0.01
+        
+    def idle_cb(self):
+        self.zoom += 0.001
+        self.px += self.vx
+        self.py += self.vy
+
+        self.area.queue_draw()
+ 
+        return True
     
     def expose_cb(self, widget, event):
         cr = widget.window.cairo_create()
@@ -96,9 +119,10 @@ class YayBeeSee(sugar.activity.activity.Activity):
             # Draw the background image.
             cr.save()
             
-            # Scale the image such that its larger access fills the screen,
-            # while preserving the aspect ratio, 
-            ratio = min(
+            # Scale the image such that it fills the screen while preserving the aspect ratio.
+            # Using 'min' here shows the entire image, 'max' fills the screen.
+            # It might be nice to be able to choose 'fill' or 'fit' mode per-image.
+            ratio = max(
                 float(bounds.width) / float(self.pixbuf.get_width()),
                 float(bounds.height) / float(self.pixbuf.get_height()))
 
@@ -108,18 +132,55 @@ class YayBeeSee(sugar.activity.activity.Activity):
             
             cr.scale(ratio, ratio)
             
+            # Apply 'Ken Burns' effect.
+            cr.translate(self.px, self.py)
+            cr.scale(self.zoom, self.zoom)
+
             cr.set_source_pixbuf(self.pixbuf, 0, 0)
             cr.paint()
             
             cr.restore()
             
             # Draw the letter.
-            text = self.key.upper() + self.key.lower()
+            if self.key.upper() != self.key.lower():
+                text = self.key.upper() + self.key.lower()
+            else:
+                text = self.key
             
             color = self.key_info['color']
             cr.set_source_rgb(color[0]/255.0, color[1]/255.0, color[2]/255.0)
             
-            cr.set_font_size(300)
+            cr.set_font_size(500)
+            x_bearing, y_bearing, width, height = cr.text_extents(text)[:4]
+            
+            cr.move_to(bounds.width - width - 30 - x_bearing, 30 - y_bearing)
+            cr.show_text(text)
+
+            # Draw the attribution.
+            text = _('Source: "%(description)s" by %(author)s') % self.key_info
+            
+            cr.set_font_size(20)
+            
+            x_bearing, y_bearing, width, height = cr.text_extents(text)[:4]
+
+            cr.set_source_rgba(0.3, 0.3, 0.3, 0.7)
+            cr.rectangle(10, bounds.height - 60 + y_bearing, width+20, height+20)
+            cr.fill_preserve()
+            cr.set_source_rgba(1, 1, 1, 0.7)
+            cr.stroke()
+            
+            cr.set_source_rgb(0.7, 0.7, 0.7)
+            cr.move_to(20, bounds.height - 50)
+            cr.show_text(text)
+
+        else:
+
+            # Draw help text.
+            text = _('Welcome! Press any letter or number.')
+            
+            cr.set_source_rgb(0.5, 0.5, 0.5)
+            
+            cr.set_font_size(20)
             x_bearing, y_bearing, width, height = cr.text_extents(text)[:4]
             
             cr.move_to(bounds.width - width/2 - 300, bounds.height - 300)
